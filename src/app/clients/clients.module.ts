afbfba2ae6113ace41a008a679860bc9bf9b866a
guillaume.gallier <guillaume.gallier@gfi.fr>
2019-10-22 17:21:14 +0200

    update client.modules

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -4,11 +4,15 @@ import {TemplatesModule} from '../templates/templates.module';
 import {SharedModule} from '../shared/shared.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ClientRoutingModule} from './client-routing.module';
+import {ListClientsComponent} from './containers/list-client/list-client.component';
+import {AddClientComponent} from './containers/add-client/add-client.component';
+import {EditClientComponent} from './containers/edit-client/edit-client.component';
+import {FormClientComponent} from './components/form-client/form-client.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListClientsComponent,AddClientComponent,EditClientComponent,FormClientComponent],
   imports: [
     CommonModule,
     ClientRoutingModule,
